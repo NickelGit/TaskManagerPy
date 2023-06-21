e4b91aac2c5be4400b107782b78d0fea12714c88
old_nickel <old_nickel@mail.ru>
2023-06-21 20:45:49 +0400

    redundant uuid removed from tag model

--- task_manager/main/models/tag.py
+++ task_manager/main/models/tag.py
@@ -1,8 +1,6 @@
-import uuid
 from django.db import models
 from .base_model import BaseModel
 
 
 class Tag(BaseModel):
     title = models.CharField(max_length=50)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
